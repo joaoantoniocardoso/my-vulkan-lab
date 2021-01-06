68f43b64d76afcb9823adc3c5c559d0f31796758
João Antônio Cardoso <joao.maker@gmail.com>
2021-01-06 12:40:36 -0300

    [Window surface]

--- vulkanTriangle/main.cpp
+++ vulkanTriangle/main.cpp
@@ -9,6 +9,7 @@
 #include <cstring> // strcmp()
 
 #include <optional> // <struct>.has_value()
+#include <set>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
@@ -68,7 +69,9 @@ class HelloTriangleApplication {
     VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
     VkDevice device;
     VkQueue graphicsQueue;
-      
+    VkQueue presentQueue;
+
+    VkSurfaceKHR surface;
 
     void initWindow() {
       glfwInit();
@@ -83,27 +86,42 @@ class HelloTriangleApplication {
     void initVulkan() {
       createInstance();
       setupDebugMessenger();
+      createSurface();
       pickPhysicalDevice();
       createLogicalDevice();
+    }
+
+    void createSurface() {
+      if (glfwCreateWindowSurface(instance, window, nullptr, &surface) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create window surface!");
+      }
     }
 
     void createLogicalDevice() {
       QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
 
-      VkDeviceQueueCreateInfo queueCreateInfo{};
-      queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
-      queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
-      queueCreateInfo.queueCount = 1;
+      std::vector<VkDeviceQueueCreateInfo> queueCreateInfos;
+      std::set<uint32_t> uniqueQueueFamilies = {
+        indices.graphicsFamily.value(),
+        indices.presentFamily.value()
+      };
 
       float queuePriority = 1.0f;
-      queueCreateInfo.pQueuePriorities = &queuePriority;
+      for (uint32_t queueFamily : uniqueQueueFamilies) {
+        VkDeviceQueueCreateInfo queueCreateInfo{};
+        queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+        queueCreateInfo.queueFamilyIndex = queueFamily;
+        queueCreateInfo.queueCount = 1;
+        queueCreateInfo.pQueuePriorities = &queuePriority;
+        queueCreateInfos.push_back(queueCreateInfo);
+      }
 
       VkPhysicalDeviceFeatures deviceFeature{};
 
       VkDeviceCreateInfo createInfo{};
       createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
-      createInfo.pQueueCreateInfos = &queueCreateInfo;
-      createInfo.queueCreateInfoCount = 1;
+      createInfo.queueCreateInfoCount = static_cast<uint32_t>(queueCreateInfos.size());
+      createInfo.pQueueCreateInfos = queueCreateInfos.data();
 
       createInfo.pEnabledFeatures = &deviceFeature;
 
@@ -121,6 +139,7 @@ class HelloTriangleApplication {
       }
 
       vkGetDeviceQueue(device, indices.graphicsFamily.value(), 0, &graphicsQueue);
+      vkGetDeviceQueue(device, indices.presentFamily.value(), 0, &presentQueue);
     }
 
     void pickPhysicalDevice() {
@@ -141,7 +160,7 @@ class HelloTriangleApplication {
       }
 
       if (physicalDevice == VK_NULL_HANDLE) {
-        throw std::runtime_error("faileld to find a suitable GPU!");
+        throw std::runtime_error("failed to find a suitable GPU!");
       }
       
     }
@@ -153,9 +172,10 @@ class HelloTriangleApplication {
 
     struct QueueFamilyIndices {
       std::optional<uint32_t> graphicsFamily;
+      std::optional<uint32_t> presentFamily;
 
       bool isComplete() {
-        return graphicsFamily.has_value();
+        return graphicsFamily.has_value() && presentFamily.has_value();
       }
     };
 
@@ -173,6 +193,14 @@ class HelloTriangleApplication {
         if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
           indices.graphicsFamily = i;
         }
+
+        VkBool32 presentSupport = false;
+        vkGetPhysicalDeviceSurfaceSupportKHR(device, i, surface, &presentSupport);
+
+        if (presentSupport) {
+          indices.presentFamily = i;
+        }
+
         if (indices.isComplete()) break;
         i++;
       }
@@ -341,6 +369,7 @@ class HelloTriangleApplication {
         DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
       }
 
+      vkDestroySurfaceKHR(instance, surface, nullptr);
       vkDestroyInstance(instance, nullptr);
 
       glfwDestroyWindow(window);
