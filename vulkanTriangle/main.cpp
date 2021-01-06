9421e13cf8c60ede08361896090599de27a0c05c
João Antônio Cardoso <joao.maker@gmail.com>
2021-01-06 11:14:08 -0300

    [Logical device and queues] End of "Setup" chapter.

--- vulkanTriangle/main.cpp
+++ vulkanTriangle/main.cpp
@@ -52,7 +52,6 @@ void DestroyDebugUtilsMessengerEXT(VkInstance instance,
 }
 
 
-
 class HelloTriangleApplication {
   public:
     void run() {
@@ -66,6 +65,10 @@ class HelloTriangleApplication {
     GLFWwindow *window;
     VkInstance instance;
     VkDebugUtilsMessengerEXT debugMessenger;
+    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+    VkDevice device;
+    VkQueue graphicsQueue;
+      
 
     void initWindow() {
       glfwInit();
@@ -81,11 +84,46 @@ class HelloTriangleApplication {
       createInstance();
       setupDebugMessenger();
       pickPhysicalDevice();
+      createLogicalDevice();
+    }
+
+    void createLogicalDevice() {
+      QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+
+      VkDeviceQueueCreateInfo queueCreateInfo{};
+      queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+      queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
+      queueCreateInfo.queueCount = 1;
+
+      float queuePriority = 1.0f;
+      queueCreateInfo.pQueuePriorities = &queuePriority;
+
+      VkPhysicalDeviceFeatures deviceFeature{};
+
+      VkDeviceCreateInfo createInfo{};
+      createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+      createInfo.pQueueCreateInfos = &queueCreateInfo;
+      createInfo.queueCreateInfoCount = 1;
+
+      createInfo.pEnabledFeatures = &deviceFeature;
+
+      createInfo.enabledExtensionCount = 0;
+
+      if (enableValidationLayers) {
+        createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+        createInfo.ppEnabledLayerNames = validationLayers.data();
+      } else {
+        createInfo.enabledLayerCount = 0;
+      }
+
+      if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create logical device");
+      }
+
+      vkGetDeviceQueue(device, indices.graphicsFamily.value(), 0, &graphicsQueue);
     }
 
     void pickPhysicalDevice() {
-      VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
-      
       uint32_t deviceCount = 0;
       vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
       if (deviceCount == 0) {
@@ -105,7 +143,7 @@ class HelloTriangleApplication {
       if (physicalDevice == VK_NULL_HANDLE) {
         throw std::runtime_error("faileld to find a suitable GPU!");
       }
-
+      
     }
 
     bool isDeviceSuitable(VkPhysicalDevice device) {
@@ -143,7 +181,7 @@ class HelloTriangleApplication {
     }
 
     void populateDebugMessengerCreateInfo(
-        VkDebugUtilsMessengerCreateInfoEXT &createInfo) {
+      VkDebugUtilsMessengerCreateInfoEXT &createInfo) {
 
       createInfo = {};
       createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
@@ -297,6 +335,8 @@ class HelloTriangleApplication {
     }
 
     void cleanup() {
+      vkDestroyDevice(device, nullptr);
+
       if (enableValidationLayers) {
         DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
       }
